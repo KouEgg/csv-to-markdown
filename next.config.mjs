ca1db380d4537093783e23f4badc62d6c913d48d
KouEgg <ktakajobs@gmail.com>
2026-05-18 16:48:05 +0900

    add redirect setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "csv-to-markdown-six.vercel.app",
+          },
+        ],
+        destination: "https://www.kouegg.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
